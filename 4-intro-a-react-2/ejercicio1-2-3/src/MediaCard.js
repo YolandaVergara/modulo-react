b83e1cddbfd58a647cd32ebcf87bc6219368562c
Yolanda <yv.avila@gmail.com>
2019-12-30 19:33:05 +0100

    add excercises 1,2 and 3 from lesson 3.4

---
+++ 4-intro-a-react-2/ejercicio1-2-3/src/MediaCard.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './stylesheets/App.css';
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
+
+class MediaCard extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="App">
+    <Header title="Yolanda Vergara" date="Lunes 16 de Diciembre de 2019"/>
+    
+    <Main text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."/>
+    <Footer />
+      </div>
+    );
+  }
+}
+
+export default MediaCard;
